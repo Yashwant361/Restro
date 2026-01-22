a410936da0c1096f2fa6a92dd967866de69862d4
yashwantray361 <yashwantray361@gmail.com>
2026-01-22 22:25:17 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,77 +1,79 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-function Navbar() {
+
+function Navbar({ closeSiderbar }) {
     return (
         <>
-            <div>
-                <nav className='bg-black text-white px-8 py-8 flex justify-between items-center'>
-                    <h1 className='text-2xl font-bold'>
+            <nav className='bg-black text-white px-8 py-8 flex justify-between items-center'>
+
+                <NavLink
+                    to='/'
+                    onClick={closeSiderbar}
+                    className='text-2xl font-bold'
+                >
+                    Foodie
+                </NavLink>
+
+
+                <ul className='flex gap-6'>
+                    <li className='flex gap-6 items-center justify-center'>
                         <NavLink
                             to='/'
+                            className={({ isActive }) =>
+                                isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
+                            }
                         >
-                            Foodie
+                            Home
                         </NavLink>
-                    </h1>
 
-                    <ul className='flex gap-6'>
-                        <li className='flex gap-6 items-center justify-center'>
-                            <NavLink
-                                to='/'
-                                className={({ isActive }) =>
-                                    isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
-                                }
-                            >
-                                Home
-                            </NavLink>
+                        <NavLink
+                            to='/menu'
+                            className={({ isActive }) =>
+                                isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
+                            }
+                        >
+                            Menu
+                        </NavLink>
 
-                            <NavLink
-                                to='/menu'
-                                className={({ isActive }) =>
-                                    isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
-                                }
-                            >
-                                Menu
-                            </NavLink>
+                        <NavLink
+                            to='/table'
+                            className={({ isActive }) =>
+                                isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
+                            }
+                        >
+                            Table
+                        </NavLink>
 
-                            <NavLink
-                                to='/table'
-                                className={({ isActive }) =>
-                                    isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
-                                }
-                            >
-                                Table
-                            </NavLink>
-                            
-                            <NavLink
-                                to='/sales'
-                                className={({ isActive }) =>
-                                    isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
-                                }
-                            >
-                                Sales
-                            </NavLink>
+                        <NavLink
+                            to='/sales'
+                            className={({ isActive }) =>
+                                isActive ? 'text-red-500 font-semibold' : 'hover:text-red-400'
+                            }
+                        >
+                            Sales
+                        </NavLink>
 
-                            <NavLink
-                                to='/contact'
-                                className={({ isActive }) =>
-                                    isActive ? 'text-red 500 font-semibold' : 'hover:text-red-400'
-                                }
-                            >
-                                Contact
-                            </NavLink>
-                            <NavLink
-                                to='/settings'
-                                className={({ isActive }) =>
-                                    isActive ? 'text-red 500 font-semibold' : 'hover:text-red-400'
-                                }
-                            >
-                                Settings
-                            </NavLink>
-                        </li>
-                    </ul>
+                        <NavLink
+                            to='/contact'
+                            className={({ isActive }) =>
+                                isActive ? 'text-red 500 font-semibold' : 'hover:text-red-400'
+                            }
+                        >
+                            Contact
+                        </NavLink>
+                        <NavLink
+                            to='/settings'
+                            className={({ isActive }) =>
+                                isActive ? 'text-red 500 font-semibold' : 'hover:text-red-400'
+                            }
+                        >
+                            Settings
+                        </NavLink>
+                    </li>
+                </ul>
 
-                </nav>
-            </div>
+            </nav>
+
         </>
     )
 }
